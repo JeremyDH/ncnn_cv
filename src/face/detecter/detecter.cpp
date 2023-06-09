b8bfe97e11b0ad15153635956255136042d1ee28
sheng_huo_123@126.com <dongyahui4@h-partners.com>
2023-06-09 19:28:47 +0800

    add coding

--- src/face/detecter/detecter.cpp
+++ src/face/detecter/detecter.cpp
@@ -3,10 +3,18 @@
 //
 #include "detecter.h"
 #include "retinaface/retinaface.h"
+#include "centerface/centerface.h"
+#include "mtcnn/mtcnn.h"
 
 namespace mirror
 {
     Detecter* RetinafaceFactory::CreateDetecter() {
         return new RetinaFace();
+    }
+    Detecter* CenterfaceFactory::CreateDetecter() {
+        return new Centerface();
+    }
+    Detecter* MtcnnfaceFactory::CreateDetecter() {
+        return new Mtcnnface();
     }
 }
