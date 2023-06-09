04b729825ef15f6c51e7f2ac4951cfcd1236d76d
sheng_huo_123@126.com <dongyahui4@h-partners.com>
2023-06-09 19:31:27 +0800

    更新代码

--- src/face/face_engine.cpp
+++ src/face/face_engine.cpp
@@ -4,55 +4,82 @@
 #include "detecter/detecter.h"
 #include "landmarker/landmarker.h"
 
-
-
-namespace mirror {
-    class FaceEngine::Impl {
+namespace mirror
+{
+    class FaceEngine::Impl{
     public:
         Impl() {
-            // detecter_factory_ = new AnticonvFactory();
             detecter_factory_ = new RetinafaceFactory();
-            landmarker_factory_ = new InsightfaceLandmarkerFactory();
-
+            detecter_factory_2 = new CenterfaceFactory();
+            detecter_factory_3 = new MtcnnfaceFactory();
+            landmarK_factory_ = new InsightfaceLandmarkerFactory();
 
             detecter_ = detecter_factory_->CreateDetecter();
-            landmarker_ = landmarker_factory_->CreateLandmarker();
+            landmarker_ = landmarK_factory_->CreateLandmaker();
+            detecter_2 = detecter_factory_2->CreateDetecter();
+            detecter_3 = detecter_factory_3->CreateDetecter();
 
-
-            initialized_ = false;
         }
 
-        ~Impl() {
-            if (detecter_) {
+        ~Impl(){
+            if(detecter_){
                 delete detecter_;
                 detecter_ = nullptr;
             }
-
-            if (landmarker_) {
-                delete landmarker_;
-                landmarker_ = nullptr;
-            }
-
-            if (detecter_factory_) {
+            if(detecter_factory_){
                 delete detecter_factory_;
                 detecter_factory_ = nullptr;
             }
 
-            if (landmarker_factory_) {
-                delete landmarker_factory_;
-                landmarker_factory_ = nullptr;
+            if(detecter_2){
+                delete detecter_2;
+                detecter_2 = nullptr;
+            }
+            if(detecter_factory_2){
+                delete detecter_factory_2;
+                detecter_factory_2 = nullptr;
             }
 
+            if(detecter_3){
+                delete detecter_3;
+                detecter_3 = nullptr;
+            }
+            if(detecter_factory_3){
+                delete detecter_factory_3;;
+                detecter_factory_3 = nullptr;
+            }
+
+            if(landmarker_){
+                delete landmarker_;
+                landmarker_ = nullptr;
+            }
+            if(landmarK_factory_){
+                delete landmarK_factory_;
+                landmarK_factory_ = nullptr;
+            }
         }
 
-        int LoadModel(const char* root_path) {
-            if (detecter_->LoadModel(root_path) != 0) {
-                std::cout << "load face detecter failed." << std::endl;
+        int LoadModel(const char* root_path)
+        {
+            if(detecter_->LoadModel(root_path) != 0)
+            {
+                std::cout << "load face detecter failed" << std::endl;
+                return 10000;
+            }
+            if(detecter_2 ->LoadModel(root_path) != 0)
+            {
+                std::cout << "load face detecter failed" << std::endl;
+                return 10000;
+            }
+            if(detecter_3 ->LoadModel(root_path) !=0 )
+            {
+                std::cout << "load face detecter3 failed" << std::endl;
                 return 10000;
             }
 
-            if (landmarker_->LoadModel(root_path) != 0) {
-                std::cout << "load face landmarker failed." << std::endl;
+            if(landmarker_->LoadModel(root_path) != 0)
+            {
+                std::cout << "load face recognizer failed." << std::endl;
                 return 10000;
             }
 
@@ -62,25 +89,31 @@ namespace mirror {
             return 0;
         }
 
-        inline int DetectFace(const cv::Mat& img_src, std::vector<FaceInfo>* faces) {
-            return detecter_->DetectFace(img_src, faces);
+        inline int DetectFace(const cv::Mat& img_src, std::vector<FaceInfo>* faces){
+            return detecter_ ->DetectFace(img_src, faces);
         }
+
         inline int ExtractKeypoints(const cv::Mat& img_src,
-                                    const cv::Rect& face, std::vector<cv::Point2f>* keypoints) {
+                                   const cv::Rect& face, std::vector<cv::Point2f>* keypoints){
             return landmarker_->ExtractKeypoints(img_src, face, keypoints);
         }
 
 
-
     private:
         DetecterFactory* detecter_factory_ = nullptr;
-        LandmarkerFactory* landmarker_factory_ = nullptr;
+        LandmarkerFactory*  landmarK_factory_ = nullptr;
+        CenterfaceFactory*  detecter_factory_2 = nullptr;
+        MtcnnfaceFactory* detecter_factory_3 = nullptr;
+
 
     private:
         bool initialized_;
         std::string db_name_;
         Detecter* detecter_ = nullptr;
+        Detecter* detecter_2 = nullptr;
+        Detecter* detecter_3 = nullptr;
         Landmarker* landmarker_ = nullptr;
+
     };
 
     FaceEngine::FaceEngine() {
@@ -88,24 +121,22 @@ namespace mirror {
     }
 
     FaceEngine::~FaceEngine() {
-        if (impl_) {
+        if(impl_) {
             delete impl_;
             impl_ = nullptr;
         }
     }
 
-    int FaceEngine::LoadModel(const char* root_path) {
+    int FaceEngine::LoadModel(const char *root_path) {
         return impl_->LoadModel(root_path);
     }
 
-    int FaceEngine::DetectFace(const cv::Mat& img_src, std::vector<FaceInfo>* faces) {
+    int FaceEngine::DetectFace(const cv::Mat &img_src, std::vector<FaceInfo> *faces) {
         return impl_->DetectFace(img_src, faces);
     }
 
-    int FaceEngine::ExtractKeypoints(const cv::Mat& img_src,
-                                     const cv::Rect& face, std::vector<cv::Point2f>* keypoints) {
-        return impl_->ExtractKeypoints(img_src, face, keypoints);
+    int FaceEngine::ExtractKeypoints(const cv::Mat& img_src, const cv::Rect& face,
+                                     std::vector<cv::Point2f> *keypoints) {
+        return  impl_->ExtractKeypoints(img_src, face, keypoints);
     }
-
-
 }
--- src/face/face_engine.h
+++ src/face/face_engine.h
@@ -9,6 +9,19 @@
 #include "../common/common.h"
 #include "opencv2/core.hpp"
 
+
+/**
+ * dllexport和 dllimport都是DLL内的关键字，即导入和导出。
+ * dllexport是在这些类、函数以及数据的申明的时候使用。用他表明这些东西可以被外部函数使用，
+ * dllexport是把DLL中的相关代码（类、函数、数据）暴露出来为其他程序使用
+ *
+ * dllimport是在外部程序需要使用DLL内相关内容时使用的关键字。当一个外部程序要使用DLL内部代码（类、函数、全局变量）时，
+ * 只需要在程序内部使用dllimport关键字声明即可。
+ *
+ * _declspec(dllexport)与_declspec(dllimport)是相互呼应，只有在DLL内部用dllexport作了声明，才能在外部函数中用dllimport导入相关的代码。
+ *
+ * **/
+
 #if defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
 #ifdef FACE_EXPORTS
 #define FACE_API __declspec(dllexport)
